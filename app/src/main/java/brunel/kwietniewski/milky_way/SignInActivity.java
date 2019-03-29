cabbe74ac7730c7b500418d26491538f5f8f8e6a
smuger <krzysiek.kwietniewski@gmail.com>
2019-03-29 21:59:39 +0000

    Initial commit

---
+++ app/src/main/java/brunel/kwietniewski/milky_way/SignInActivity.java
@@ -0,0 +1,12 @@
+package brunel.kwietniewski.milky_way;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SignInActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(android.R.layout.activity_list_item);
+    }
+}
