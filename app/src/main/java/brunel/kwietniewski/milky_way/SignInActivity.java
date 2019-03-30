d1013f896938bd827bc9ae16ecdc033802323b9e
smuger <krzysiek.kwietniewski@gmail.com>
2019-03-30 17:34:08 +0000

    colors correction

--- app/src/main/java/brunel/kwietniewski/milky_way/SignInActivity.java
+++ app/src/main/java/brunel/kwietniewski/milky_way/SignInActivity.java
@@ -7,6 +7,6 @@ public class SignInActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(android.R.layout.activity_list_item);
+        setContentView(R.layout.activity_sign_in);
     }
 }
